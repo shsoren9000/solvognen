0f62c925852829d6e01fcc5a06d7c061c65285a2
shsoren9000 <soerenhoeyner@gmail.com>
2022-05-18 20:36:28 +0200

    working scrub animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+gsap.registerPlugin(ScrollTrigger);
 
-tl.to(".text", {y: "-100%", duration: 1})
+// const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+// gsap.to(".text", {
+//     x: 400,
+//     rotation: 360, 
+//     duration: 3,
+// });
+
+gsap.to(".text-2", {
+    scrollTrigger: {
+        trigger: ".text-2",
+        start: "20px 80%",
+        markers: true,
+        scrub: 1,
+    },
+    x: 400,
+    rotation: 360, 
+    duration: 3,
+});
+
+// gsap.to(".text-3", {
+//     scrollTrigger: ".text-3",
+//     x: 400, 
+//     rotation: 360,
+//     duration: 3,
+// });
