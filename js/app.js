22458672e7dcc83f0b857c4f05e047fa304c5c2e
shsoren9000 <soerenhoeyner@gmail.com>
2022-05-22 00:16:55 +0200

    22.05

--- js/app.js
+++ js/app.js
@@ -1,28 +1,64 @@
 gsap.registerPlugin(ScrollTrigger);
 
+
+gsap.to(".scroll", {
+    y: 10,
+    opacity: 0,
+    duration: 3,
+    repeat: -1,
+    EASE <---!
+});
+
+// // TEST AF ANIMATION
 // const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-// gsap.to(".text", {
+// gsap.to(".scroll", {
 //     x: 400,
 //     rotation: 360, 
 //     duration: 3,
 // });
 
-gsap.to(".text-2", {
-    scrollTrigger: {
-        trigger: ".text-2",
-        start: "20px 80%",
-        markers: true,
-        scrub: 1,
-    },
-    x: 400,
-    rotation: 360, 
-    duration: 3,
-});
-
-// gsap.to(".text-3", {
-//     scrollTrigger: ".text-3",
-//     x: 400, 
-//     rotation: 360,
+// gsap.to(".text", {
+//     scrollTrigger: {
+//         trigger: ".text",
+//         start: "top",
+//         markers: true,
+//         scrub: 1,
+//     },
+//     x: 400,
+//     rotation: 360, 
 //     duration: 3,
 // });
+
+// gsap.to(".text-2", {
+//     scrollTrigger: {
+//         trigger: ".text-2",
+//         // start: "20px 80%",
+//         markers: true,
+//         scrub: 1,
+//     },
+//     x: 400,
+//     rotation: 360, 
+//     duration: 3,
+// });
+
+// gsap.to(".text-3", {
+//     scrollTrigger: {
+//         trigger: ".text-3",
+//         start: "20px 80%",
+//         markers: true,
+//         scrub: 1,
+//     },
+//     x: 400,
+//     rotation: 360, 
+//     duration: 3,
+// });
+
+// gsap.fromTo(".text-4", { opacity: 0, duration: 10 }, { opacity: 1, duration: 10, }, )
+
+// // gsap.to(".text-3", {
+// //     scrollTrigger: ".text-3",
+// //     x: 400, 
+// //     rotation: 360,
+// //     duration: 3,
+// // });
